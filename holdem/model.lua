d3410c2d339261c1fd77faa1e559211dee8245ec
Kingo Wang <wanggenguo@outlook.com>
2015-08-18 16:32:46 +0800

    add model

---
+++ holdem/model.lua
@@ -0,0 +1,42 @@
+require 'torch'   -- torch
+require 'image'   -- for image transforms
+require 'nn'      -- provides all sorts of trainable modules/layers
+
+-- 14-class problem
+noutputs = 14
+
+-- input dimensions
+nfeats = 3
+width = 32
+height = 32
+ninputs = nfeats*width*height
+
+-- number of hidden units (for MLP only):
+nhiddens = ninputs / 2
+
+-- hidden units, filter sizes (for ConvNet only):
+nstates = {64,64,128}
+filtsize = 5
+poolsize = 2
+normkernel = image.gaussian1D(7)
+
+-- if opt.model == 'linear' then
+
+   -- Simple linear model
+   model = nn.Sequential()
+   model:add(nn.Reshape(ninputs))
+   model:add(nn.Linear(ninputs,noutputs))
+
+--[[
+elseif opt.model == 'mlp' then
+
+   -- Simple 2-layer neural network, with tanh hidden units
+   model = nn.Sequential()
+   model:add(nn.Reshape(ninputs))
+   model:add(nn.Linear(ninputs,nhiddens))
+   model:add(nn.Tanh())
+   model:add(nn.Linear(nhiddens,noutputs))
+--]]
+--end
+
+print(model)
